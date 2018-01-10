afebc7f6c8cfbc9fee454d0af3e26ec16ea80c66
InFi <take5079@gmail.com>
2018-01-11 00:01:18 +0900

    Weapon Proto 추가
    현재 마우스 초점 불안정 수정필요

---
+++ Revolution/Assets/MiddleGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleGun : MonoBehaviour {
+
+    RaycastHit hit;
+
+    [SerializeField]
+    Transform shootPoint;
+
+    [SerializeField]
+    int currentAmmo;
+
+
+    //Rate Od Fire
+    [SerializeField]
+    float RateOffFire;
+    float NextFire = 0;
+
+    [SerializeField]
+    float weponRange;
+
+
+    void Update()
+    {
+        //Fire1은 Edit->Project Setting 에서 세팅된 값을 사용한 것(수정가능)
+        if (Input.GetButton("Fire1") && currentAmmo > 0)
+        {
+            Shoot();
+        }
+    }
+
+
+    //총 쏘기
+    void Shoot()
+    {
+        if (Time.time > NextFire)
+        {
+            NextFire = Time.time + RateOffFire;
+
+            currentAmmo--;
+
+
+            if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, weponRange))
+            {
+                if (hit.transform.tag == "Enemy")
+                {
+                    Debug.Log("Hit Enemy");
+                }
+                else
+                {
+                    Debug.Log("Hit Something Else");
+                }
+            }
+        }
+    }
+}

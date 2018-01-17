918914bad8caa99b8a038e22c0fcf5ff56cfa1cd
InFi <take5079@gmail.com>
2018-01-17 17:30:32 +0900

    총알 구현중, 파티클 추가예정

---
+++ Revolution/Assets/Tutorial Stage/Scripts/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public PlayerWeapon Weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    { 
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, Weapon.range, mask))
+        {
+            Debug.Log("We hit" + _hit.collider.name);
+        }
+    }
+}

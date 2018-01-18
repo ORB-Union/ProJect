50e2e7584643c882f29082c5466500739f2b9f76
InFi <take5079@gmail.com>
2018-01-18 17:40:08 +0900

    총 발사 구현 및 총구화염, 탄흔 효과 프로토 추가
    추후 총구화염, 탄흔효과 수정할 예정

---
+++ Revolution/Assets/Tutorial Stage/Scripts/PlayerShoot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public PlayerWeapon Weapon;
+
+    [SerializeField]
+    private Camera cam;
+    public ParticleSystem MiddleGunmuzzleFlash;
+    public GameObject ImpactEffect;
+    public float fireRate = 15.0f;
+    public float impactforce = 30.0f;
+
+    private float nextTimeToFire = 0f;
+
+    //[SerializeField]
+    //private LayerMask mask;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        MiddleGunmuzzleFlash.Play();
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, Weapon.range))
+        {
+            Debug.Log("We hit" + _hit.collider.name);
+
+            Target target = _hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(Weapon.damage);
+            }
+
+            if (_hit.rigidbody != null)
+            {
+                _hit.rigidbody.AddForce(-_hit.normal * impactforce);
+            }
+
+            GameObject impactGo = Instantiate(ImpactEffect, _hit.point, Quaternion.LookRotation(_hit.normal));
+            Destroy(impactGo, 2.0f);
+        }
+    }
+}
---
+++ Revolution/Assets/Tutorial Stage/Scripts/PlayerWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerWeapon{
+
+    public string WeaponName = "RayGun";
+    public float damage = 10.0f;
+    public float range = 100.0f;
+
+}
